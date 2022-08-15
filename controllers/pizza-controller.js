42418bc976a9f080192f0e4e61f9fdc00954d855
jesse sheehan <jessetsheehan@gmail.com>
2022-08-14 21:20:39 -0400

    finished creating user model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -13,7 +13,7 @@ const pizzaController = {
     },
 
     //get one pizza by id
-    getPizzaByID({ params }, res) {
+    getPizzaById({ params }, res) {
         Pizza.findOne({_id: params.id})
             .then(dbPizzaData => {
                 //if no pizza is found, send 404
